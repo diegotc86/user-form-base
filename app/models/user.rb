9f0f39db611cf4ecf4a7879594f329cb5396e500
Brayan Ciudad <linzeur@gmail.com>
2019-04-09 17:09:53 -0500

    Create User complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,27 @@
 class User < ApplicationRecord
+  validates :first_name, :last_name, length: { in: 2..60 }
+  validates :username, uniqueness: true, length: { in: 2..20 }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, confirmation: true
+  validates :password, format: { with: /.*[0-9].*/, message: "has to at least one number" }, length: { minimum: 8 }
+  validates :email_confirmation, :password_confirmation, presence: true
+  validates :password_hint, length: { maximum: 100 }
+  before_save :capitalize_first_name, :capitalize_last_name
+
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def capitalize_first_name
+    self.first_name.capitalize
+  end
+
+  def capitalize_last_name
+    self.last_name.downcase.split.map do |word|
+      if ["del", "la", "de", "las"].include?(word)
+        word
+      else
+        word.capitalize
+      end
+    end.join(" ")
   end
 end
