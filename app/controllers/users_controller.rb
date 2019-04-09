47d71b4f569d12fda3179ff5125ab555a8464c57
Rixele <liamrn94@gmail.com>
2019-04-09 18:59:50 -0500

    lian nivin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(params[:user])
+    @user = User.create(first_name: params[:user][:first_name])
+    @user = User.create(last_name: params[:user][:last_name])
+    @user = User.create(username: params[:user][:username])
+    @user = User.create(email: params[:user][:email])
+    @user = User.create(password: params[:user][:password])
+    @user = User.create(password_hint: params[:user][:password_hint])
+    # @user = User.create(params[:user])
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,37 @@
 class User < ApplicationRecord
-  def full_name
-    "#{first_name} #{last_name}"
-  end
+  # Must be between 2 and 60 characters
+  # validates :first_name,
+              # length: { in: 2..60 }
+
+  # Must be between 2 and 60 characters          
+  # validates :last_name, 
+              # length: { in: 2..60 }
+
+  # Must be unique. Must be between 2 and 20 characters
+  # validates :username, 
+            # {uniqueness: true, length: { in: 2..20 }}
+
+  # Must be unique. Must be a valid email. Must be the same as the email. (FALTA VALID EMAIL)
+  # validates :email, 
+              # confirmation: { case_sensitive: false },
+              # uniqueness: true, 
+              # format: { with: /\A[a-zA-Z0-9]+\z/}, 
+              # confirmation: true
+  
+  # At least 8 characters. Must contain at least 1 number. Must the same as the password. (FALTA 1+ NUMEROS)
+  # validates :password, 
+  #           length: { minimum: 8 }, 
+  #           format: { with: /\A[a-zA-Z0-9][a-zA-Z0-9_]*\z/ }, 
+  #           confirmation: true
+
+  # Optional. Must be 100 characters max.
+  # validates :password_hint, 
+  #             presence: false,
+  #             length: { maximum: 100 }
+
+
+
+
+#   def full_name
+#     "#{first_name} #{last_name}"
 end
