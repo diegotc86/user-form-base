13a4aab88995bd7e30eb46f2ae88d5ae5a14a8dc
Cesar Cachay <cesarcachay1996@gmail.com>
2019-04-09 19:48:22 -0500

    Show errors for empty spaces

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,21 @@ class UsersController < ApplicationController
   end
 
   def new
+    @errors = "Show me the errors dude!"
     @user = User.new
   end
 
   def create
-    @user = User.create(params[:user])
+    @user = User.create(params[:user_params])
+
+    if @user.valid?
+      @user.save
+      redirect_to "/users/new", status: 301
+    else
+      errors = @user.errors.full_messages
+      redirect_to "/users/new", status: 301, flash: { errors: errors}
+    end
+
   end
 
   private
@@ -25,4 +35,15 @@ class UsersController < ApplicationController
       :password_hint
     )
   end
+
+  # def valitadate_last_name(my_last_name)
+  #   temporal = my_last_name.downcase.split(' ')
+  #   temporal.each_with_index do |c|
+  #     if c.length >= 3
+  #       if c.[0.1] != "d"
+  #         c.capitalize!
+  #       end
+  #     end
+  #   end
+  #   temporal.join(' ')
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-  validates_length_of :first_name, presence: true,  minimum: 2, maximum: 60
-  validates_length_of :last_name, presence: true,  minimum: 2, maximum: 60
-  validates :username, presence: true, uniqueness: true, minimum: 2, maximum: 20
-  validates :email, presence: true, confirmation: true, uniqueness: true, format: { with: [a-zA-Z0-9]+[a-zA-Z0-9]+{2,3} }
+  validates :first_name, presence: true, length: { in: 2..60 }
+  validates :last_name, presence: true, length: { in: 2..60 } #alow_nill is u want this space in blank
+  validates :username, presence: true, uniqueness: true, length: { in: 2..20 }
+  validates :email, presence: true, confirmation: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ } ##[a-zA-Z0-9]+[a-zA-Z0-9]+[2.3]
   validates :email_confirmation,  presence: true
-  validates :password, presence: true, minimum: 8, confirmation: true, format: { with: [0-9] }
+  validates :password, presence: true, length: { minimum: 8 }, confirmation: true, format: { with: /.*[0-9].*/ }
   validates :password_confirmation, presence: true
-  validates :hint, maximum: 100
+  validates :password_hint, length: { maximum: 100 } 
 
 
   def full_name
