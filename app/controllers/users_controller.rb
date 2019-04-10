0e60edd10ee8909d7f4d4e543ce79b590d109dbd
CesarCachay <44324282+CesarCachay@users.noreply.github.com>
2019-04-10 09:16:30 -0500

    Revert "Form day2"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,21 +4,11 @@ class UsersController < ApplicationController
   end
 
   def new
-    @errors = "Show me the errors dude!"
     @user = User.new
   end
 
   def create
-    @user = User.create(params[:user_params])
-
-    if @user.valid?
-      @user.save
-      redirect_to "/users/new", status: 301
-    else
-      errors = @user.errors.full_messages
-      redirect_to "/users/new", status: 301, flash: { errors: errors}
-    end
-
+    @user = User.create(params[:user])
   end
 
   private
@@ -35,15 +25,4 @@ class UsersController < ApplicationController
       :password_hint
     )
   end
-
-  # def valitadate_last_name(my_last_name)
-  #   temporal = my_last_name.downcase.split(' ')
-  #   temporal.each_with_index do |c|
-  #     if c.length >= 3
-  #       if c.[0.1] != "d"
-  #         c.capitalize!
-  #       end
-  #     end
-  #   end
-  #   temporal.join(' ')
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,5 @@
 class User < ApplicationRecord
-  validates :first_name, presence: true, length: { in: 2..60 }
-  validates :last_name, presence: true, length: { in: 2..60 } #alow_nill is u want this space in blank
-  validates :username, presence: true, uniqueness: true, length: { in: 2..20 }
-  validates :email, presence: true, confirmation: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ } ##[a-zA-Z0-9]+[a-zA-Z0-9]+[2.3]
-  validates :email_confirmation,  presence: true
-  validates :password, presence: true, length: { minimum: 8 }, confirmation: true, format: { with: /.*[0-9].*/ }
-  validates :password_confirmation, presence: true
-  validates :password_hint, length: { maximum: 100 } 
-
-
   def full_name
     "#{first_name} #{last_name}"
   end
-
-
 end
