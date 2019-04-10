d20f217a63d22854be66e368dcbbc199d0a4e2b7
Lian Nivin <liamrn94@gmail.com>
2019-04-10 06:37:50 -0500

    Add routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,35 +1,35 @@
 class User < ApplicationRecord
   # Must be between 2 and 60 characters
-  # validates :first_name,
-              # length: { in: 2..60 }
+  validates :first_name,
+            length: { in: 2..60 }
 
   # Must be between 2 and 60 characters          
-  # validates :last_name, 
-              # length: { in: 2..60 }
+  validates :last_name, 
+            length: { in: 2..60 }
 
   # Must be unique. Must be between 2 and 20 characters
-  # validates :username, 
-            # {uniqueness: true, length: { in: 2..20 }}
+  validates :username, 
+            {uniqueness: true, length: { in: 2..20 }}
 
-  # Must be unique. Must be a valid email. Must be the same as the email. (FALTA VALID EMAIL)
-  # validates :email, 
-              # confirmation: { case_sensitive: false },
-              # uniqueness: true, 
-              # format: { with: /\A[a-zA-Z0-9]+\z/}, 
-              # confirmation: true
+  # Must be unique. Must be a valid email. Must be the same as the email.
+  # (FALTA VALIDAR EMAIL)
+  validates :email, 
+            confirmation: { case_sensitive: false },
+            uniqueness: true, 
+            format: { with: /\A[a-zA-Z0-9]+\z/}, 
+            confirmation: true
   
-  # At least 8 characters. Must contain at least 1 number. Must the same as the password. (FALTA 1+ NUMEROS)
-  # validates :password, 
-  #           length: { minimum: 8 }, 
-  #           format: { with: /\A[a-zA-Z0-9][a-zA-Z0-9_]*\z/ }, 
-  #           confirmation: true
+  # At least 8 characters. Must contain at least 1 number. Must the same as the password. 
+  # (FALTA VALIDAR 1+ NUMEROS)
+  validates :password, 
+             length: { minimum: 8 }, 
+             format: { with: /\A[a-zA-Z0-9][a-zA-Z0-9_]*\z/ }, 
+             confirmation: true
 
-  # Optional. Must be 100 characters max.
-  # validates :password_hint, 
-  #             presence: false,
-  #             length: { maximum: 100 }
-
-
+  #Optional. Must be 100 characters max.
+  validates :password_hint, 
+            presence: false,
+            length: { maximum: 100 }
 
 
 #   def full_name
